0de0c0fc0a5cab06263983c726163c1b2bd6bcd0
rstott87 <rstott87@yahoo.com>
2022-07-06 23:38:57 -0400

    add main image/ add title text to containter

--- src/components/MainSetion.jsx
+++ src/components/MainSetion.jsx
@@ -25,8 +25,14 @@ function MainSection() {
 
     return (
         <div className="main-section flex justify-center bg-gradient-to-r from-blue-600 to-blue-300">
-            <div className="my-36 text-bold text-neutral-50">
-                <div className="text-3xl m-20">DAVE FOLEY</div>
+            <div className="text-bold text-neutral-50">
+            <div className="p-5">
+                <p className="text-3xl" >Dave Foley</p>
+                <p className="text-lg"> For Local President</p>
+            </div>
+                <div className="backdrop-invert-40 p-5 ">
+                    <img className= "opacity-60"src="./src/images/Dave_headshot.jpeg"></img>
+                </div>
                 <p className="text-center font-bold text-2xl">Join Our Campaign</p>
                 <p className="text-center text-sm">For info and upates</p>
                 <EmailForm getEmail={userEmail} />
