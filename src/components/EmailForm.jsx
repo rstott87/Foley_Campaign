5af2b52d25dd5259795f1dc06023689cc3e54d73
rstott87 <rstott87@yahoo.com>
2022-07-10 17:32:39 -0400

    add EmailJS to email state

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -1,23 +1,41 @@
 import { useState } from "react";
 import Button from "./UI/Button";
+import { send } from 'emailjs-com';
 
 function EmailForm(props) {
     const [enteredname, setEnteredName] = useState("");
     const [enteredEmail, setEnteredEmail] = useState("");
     const [enteredAgency, setEnteredAgency] = useState("");
+    const [enteredPhone, setEnteredPhone] = useState("");
+
+    const [toSend, setToSend] = useState({
+        from_name: 'Bob',
+        to_name: 'Dave',
+        message: "The user info is as follows: ",
+        reply_to: 'bob',
+      });
 
 
     const emailChangeHandler = (e) => {
         setEnteredEmail(e.target.value);
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
     }
 
     const nameChangeHandler = (e) => {
         setEnteredName(e.target.value);
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
     }
 
     const agencyChangeHandler = (e) => {
         setEnteredAgency(e.target.value);
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
     }
+
+    const phoneChangeHandler = (e) => {
+        setEnteredPhone(e.target.value);
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
+    }
+
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -26,10 +44,24 @@ function EmailForm(props) {
             email: enteredEmail,
             agency: enteredAgency
         }
+        send(
+            'service_mr00i87',
+            'template_9n3a95t',
+            toSend,
+            'Lm-3BLvvZtqU2-L81'
+          )
+            .then((response) => {
+              console.log('SUCCESS!', response.status, response.text);
+            })
+            .catch((err) => {
+              console.log('FAILED...', err);
+            });
+        
         props.getEmail(voter);
         setEnteredName("")
         setEnteredEmail("")
         setEnteredAgency("")
+        setEnteredPhone("")
     }
 
     let joinCampaign = ""
@@ -50,6 +82,7 @@ function EmailForm(props) {
                 <div className="input-holder col-span-2">
                     <input
                         className="rounded-md box-border border border-slate-400 pl-3 min-w-full h-11 text-black"
+                        name="name"
                         type="text"
                         onChange={nameChangeHandler}
                         value={enteredname}
@@ -59,19 +92,32 @@ function EmailForm(props) {
                 <div className="input-holder col-span-2">
                     <input
                         className="rounded-md box-border border border-slate-400 pl-3 min-w-full h-11 text-black"
+                        name="email"
                         type="text"
                         onChange={emailChangeHandler}
                         value={enteredEmail}
                         placeholder={"Email*"}
                     />
                 </div>
+                <div className="input-holder col-span-2">
+                    <input
+                        className="rounded-md box-border border border-slate-400 pl-3 min-w-full h-11 text-black"
+                        name="phone"
+                        type="tel"
+                        onChange={phoneChangeHandler}
+                        value={enteredPhone}
+                        placeholder={"Phone"}
+                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                    />
+                </div>
                 <div className="input-holder ">
                     <input
                         className="rounded-md border border-slate-400 pl-2 w-full text-black h-11"
+                        name="chapter"
                         type="text"
                         onChange={agencyChangeHandler}
                         value={enteredAgency}
-                        placeholder={"Agency"}
+                        placeholder={"Chapter"}
                     />
                 </div>
                 <div className="rounded-md box-border border border-slate-500 flex bg-blue-600 justify-center text-white font-semibold">
--- src/components/UI/Logo.jsx
+++ src/components/UI/Logo.jsx
@@ -1,7 +1,7 @@
 
 const Logo = function (props) {
     let name = "Dave";
-    
+
     if (props.language === "English") {
         name = "Dave"
     }
@@ -11,24 +11,24 @@ const Logo = function (props) {
 
 
 
-return (
-    <div className="drop-shadow-2xl mx-auto mb-10 mt-8 w-48 font-bold p-2 text-neutral-100">
-        <div className="flex justify-evenly">
-            <p className="w-full bg-white border border-blue-900 text-xl font-extrabold fon text-center tracking-wide text-slate-600 -rotate-6 shadow-md shadow-slate-800">ELECT</p>
-            <p className=" w-full text-center font-black tracking-wider text-2xl rotate">DAVE </p>
+    return (   
+            <div className="mx-auto mb-10 mt-8 w-48 font-bold p-2 text-neutral-100">
+                <div className="flex justify-evenly">
+                    <p className="w-full bg-white border border-blue-900 text-xl font-extrabold fon text-center tracking-wide text-slate-600 -rotate-6 shadow-md shadow-slate-800">ELECT</p>
+                    <p className=" w-full text-center font-black tracking-wider text-2xl rotate">DAVE </p>
 
 
-        </div>
-        <p className="mt-1 text-neutral font-black tracking-wider text-center text-5xl">FOLEY</p>
-        <div className="mt-2 flex">
-            <div className="w-full  bg-white h-1"></div>
-            <div className="text-4xl mx-2 leading-4">**</div>
-            <div className=" w-full bg-white h-1"></div>
-        </div>
-        <p className="text-center text-lg leading-4">Local 509</p>
-        <p className="text-center tracking-wider text-2xl" >President</p>
-    </div>
-)
+                </div>
+                <p className="mt-1 text-neutral font-black tracking-wider text-center text-5xl">FOLEY</p>
+                <div className="mt-2 flex">
+                    <div className="w-full  bg-white h-1"></div>
+                    <div className="text-4xl mx-2 leading-4">**</div>
+                    <div className=" w-full bg-white h-1"></div>
+                </div>
+                <p className="text-center text-lg leading-4">Local 509</p>
+                <p className="text-center tracking-wider text-2xl" >President</p>
+            </div>
+    )
 }
 
 export default Logo; 
