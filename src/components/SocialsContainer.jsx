1d71dd3d31db879f2af5d8d460387c0a4a38cabc
rstott87 <rstott87@yahoo.com>
2022-07-11 17:32:26 -0400

    socials adjustment

--- src/components/SocialsContainer.jsx
+++ src/components/SocialsContainer.jsx
@@ -1,19 +1,27 @@
 import SocialsItem from "./UI/SocialsItem";
 function SocialsContainer() {
   return (
-    <div className="flex w-6/12 justify-evenly py-2 ">
-      <div className="h-min object-contain max-h-1">
+    <div className="flex w-5/12 justify-evenly py-2 ">
+      <div className=" ">
         <img
-          className=""
+          className="h-10 object-scale-down"
           src="src/images/icons8-instagram-50.png"
           alt=""
         />
       </div>
       <div className="h-min object-contain">
-        <img src="src/images/icons8-facebook-50.png" alt="" />
+        <img
+          className="h-10 object-scale-down"
+          src="src/images/icons8-facebook-50.png"
+          alt=""
+        />
       </div>
       <div className="h-min object-contain">
-        <img src="src/images/icons8-twitter-50.png" alt="" />
+        <img
+          className="h-10 object-scale-down"
+          src="src/images/icons8-twitter-50.png"
+          alt=""
+        />
       </div>
     </div>
   );
