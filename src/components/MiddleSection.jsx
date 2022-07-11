59e0328e9d2e3e42d0f25b6fbf4d8ed509da47d5
rstott87 <rstott87@yahoo.com>
2022-07-11 09:01:40 -0400

    button adjust colors

--- src/components/MiddleSection.jsx
+++ src/components/MiddleSection.jsx
@@ -12,11 +12,11 @@ const MiddleSection = function (props) {
     }
 
     return (
-        <div className='flex justify-between px-8 py-6 bg-gradient-to-r from-blue-200 to-blue-100'>
-            <button class="shadow-md shadow-slate-800 w-full bg-violet-600 text-white font-bold py-4 px-6 rounded-md">
+        <div className='flex justify-evenly px-8 py-6 bg-gradient-to-r w-full from-blue-200 to-blue-100'>
+            <button class="shadow-md shadow-slate-800 w-48 bg-violet-600 text-white font-bold py-4 px-6 rounded-md">
                 CONTRIBUTE
             </button>
-            <button class="shadow-md shadow-slate-800 w-full ml-2 bg-violet-600 text-white font-bold py-4 px-6 rounded-md"
+            <button class="shadow-md shadow-slate-800 ml-3 bg-violet-600 w-36 text-white font-bold py-4 px-6 rounded-md"
                 onClick={props.clickHandler}>
                 {buttonLanguage}
             </button>
--- src/components/UI/JoinButton.jsx
+++ src/components/UI/JoinButton.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function JoinButton(props) {
 
     return (
-        <div className={`shadow-md shadow-slate-700 rounded-md flex ${props.isDisabled?'bg-blue-400':'bg-blue-600'} justify-center text-white font-semibold`}>
+        <div className={`shadow-md shadow-slate-800 rounded-md flex ${props.isDisabled?'bg-blue-400':'bg-blue-800'} justify-center text-white font-semibold`}>
             <button type='submit'>
                 JOIN US
             </button>
