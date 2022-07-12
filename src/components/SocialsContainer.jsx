7a36ed4057ef4474c5b62c676eb16089092e0dbb
rstott87 <rstott87@yahoo.com>
2022-07-11 20:09:28 -0400

    make facebook logo link to github

--- src/components/SocialsContainer.jsx
+++ src/components/SocialsContainer.jsx
@@ -12,7 +12,7 @@ function SocialsContainer() {
       <div className="h-min object-contain">
         <img
           className="h-10 object-scale-down"
-          src="src/images/icons8-facebook-50.png"
+          src="https://github.com/rstott87/Foley_Campaign/blob/64d6a562dbc1c7d307149584f456946f673aa2c6/src/images/icons8-facebook-50.png"
           alt=""
         />
       </div>
