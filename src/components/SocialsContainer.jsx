63955352dc4cf9cacb7381cc870e8045270507ee
rstott87 <rstott87@yahoo.com>
2022-07-11 20:25:45 -0400

    remove object-scale-down from social logos to to see if images now render in prod

--- src/components/SocialsContainer.jsx
+++ src/components/SocialsContainer.jsx
@@ -4,21 +4,21 @@ function SocialsContainer() {
     <div className="flex w-5/12 justify-evenly p-1">
       <div className=" ">
         <img
-          className="h-10 object-scale-down"
+          className=""
           src="src/images/icons8-instagram-50.png"
           alt=""
         />
       </div>
       <div className="h-min object-contain">
         <img
-          className="h-10 object-scale-down"
-          src="https://github.com/rstott87/Foley_Campaign/blob/64d6a562dbc1c7d307149584f456946f673aa2c6/src/images/icons8-facebook-50.png"
+          className=""
+          src="src/images/icons8-facebook-50.png"
           alt=""
         />
       </div>
       <div className="h-min object-contain">
         <img
-          className="h-10 object-scale-down"
+          className=""
           src="src/images/icons8-twitter-50.png"
           alt=""
         />
