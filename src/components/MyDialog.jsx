560560f656dfac6776a8c75ff1b478d189fd85f6
rstott87 <rstott87@yahoo.com>
2022-07-18 13:03:47 -0400

    dialog box is set up background so user does not click when box is open

--- src/components/MyDialog.jsx
+++ src/components/MyDialog.jsx
@@ -9,18 +9,18 @@ function MyDialog(props) {
       onClose={() => props.contributeClick}
     >
       <div className="fixed inset-0 flex items-center justify-center p-4">
-        <Dialog.Panel className="w-full max-w-sm rounded-xl border-4 border-red-700 bg-white p-4 text-2xl">
-          <Dialog.Title className="text-center">Choose a Way to Contribute</Dialog.Title>
-
-          <div className="flex w-full justify-center">
-            <button
-              className="mt-10 text-lg w-24 rounded-md bg-violet-600 p-2 font-bold text-white shadow-md shadow-slate-800"
-              onClick={props.contributeClick}
-            >
-              Cancel
-            </button>
-          </div>
-        </Dialog.Panel>
+          <div className="absolute z-0 h-screen w-screen bg-slate-800 opacity-90 "></div>
+          <Dialog.Panel className="z-50 w-full max-w-sm rounded-xl border-4 border-red-700 bg-white p-4 text-2xl">
+            <Dialog.Title className="text-center">Choose a Way to Contribute</Dialog.Title>
+            <div className="flex w-full justify-center">
+              <button
+                className="mt-10 text-lg w-24 rounded-md bg-violet-600 p-2 font-bold text-white shadow-md shadow-slate-800"
+                onClick={props.contributeClick}
+              >
+                Cancel
+              </button>
+            </div>
+          </Dialog.Panel>
       </div>
     </Dialog>
   );
