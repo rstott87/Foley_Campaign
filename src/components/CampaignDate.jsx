bc4955410ea7e25b85091c9b1a6f16049f2edd2a
rstott87 <rstott87@yahoo.com>
2022-09-09 15:33:06 -0400

    created campaign date component and added text

---
+++ src/components/CampaignDate.jsx
@@ -0,0 +1,12 @@
+
+
+function campaignDate() {
+    return (
+      <div>
+        <p>Days until the election</p>
+        <p>learn how to vote</p>
+      </div>
+    );
+}
+
+export default campaignDate;
