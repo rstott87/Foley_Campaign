601be92db8505349875f1ff240f4fcc851ac900f
rstott87 <rstott87@yahoo.com>
2022-09-09 16:26:27 -0400

    deleted line

--- src/components/CampaignDate.jsx
+++ src/components/CampaignDate.jsx
@@ -4,7 +4,6 @@ function campaignDate() {
     return (
       <div className="z-10">
         <p>Ballots Get Mailed on November 7th</p>
-        <p>Click here to learn more about voting</p>
       </div>
     );
 }
