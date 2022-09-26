2c02145a0e40dd8f8c19f4bbb448f0277526a8cc
rstott87 <rstott87@yahoo.com>
2022-09-26 14:33:28 -0400

    animated hamburger menu

--- src/components/UI/MenuButton.jsx
+++ src/components/UI/MenuButton.jsx
@@ -1,22 +1,27 @@
+//animates hamburger bar to turn itno x if menuOpen state is true
 function MenuButton(props) {
   return (
     <div className="menu-container my-1 w-12" onClick={props.menuClickHandler}>
       <div
         className={`h-2 w-full rounded-md bg-slate-700  ${
-          props.menuOpen ? "rotate-45 duration-500" : "rotate-0 duration-500"
+          props.menuOpen
+            ? "origin-top-left rotate-45 duration-500"
+            : "rotate-0 duration-500"
         }`}
       ></div>
       <div
         className={`my-2 h-2 w-full rounded-md bg-slate-700
       ${
         props.menuOpen
-          ? "bg-transparent duration-500 "
-          : "bg-slate-700 duration-500"
+          ? "bg-transparent duration-300 "
+          : "bg-slate-700 duration-300"
       } `}
       ></div>
       <div
         className={`h-2 w-full rounded-md bg-slate-700  ${
-          props.menuOpen ? "-rotate-45 duration-500" : "rotate-0 duration-500"
+          props.menuOpen
+            ? "origin-bottom-left -rotate-45 duration-500"
+            : "rotate-0 duration-500"
         }`}
       ></div>
     </div>
