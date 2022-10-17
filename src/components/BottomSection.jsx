4770f52f1f0fdb6775a2764d31629b80e2ac96e4
rstott87 <rstott87@yahoo.com>
2022-10-17 09:25:03 -0400

    added actual text to about me

--- src/components/BottomSection.jsx
+++ src/components/BottomSection.jsx
@@ -8,15 +8,42 @@ const BottomSection = function () {
       /> */}
       <div
         id="about"
-        className="z-30 opacity-80 bg-gradient-to-b from-neutral-100 to-violet-300 "
+        className="z-30 bg-gradient-to-b from-neutral-100 to-violet-300 opacity-80 "
       >
         <div className="m-auto w-96 py-5 px-8 lg:w-auto">
           <h1 className="mb-2 text-center text-3xl font-bold leading-loose">
             About Dave
           </h1>
-          <p className="text-center text-lg font-bold italic tracking-wide lg:tracking-tight">
-            Dave has been an employee with the Department of Revenue's Child Support Enforcement Division for over 10 years. Not long after his career began at DOR, he became actively involved with SEIU as a union steward. Dave's reputation as an attentive and accesible steward led to decisve victories in both of his campaigns for chapter president, a position which he currently holds. As chapter president, he's become known as someone who's easy to reach and quick to get things done.
-          </p>
+          <div className="text-lg font-bold italic tracking-wide lg:tracking-tight">
+            <p className="indent-5">
+              When we fight, we win! But to win, we actually need to fight. That
+              means organizing actions at our job sites, which requires a
+              network of well-trained and engaged stewards and leaders
+              throughout the state. Our Local has only recently begun to
+              prioritize our stewards. I promise that as President we will fully
+              commit to training stewards and rank-and-file members to run our
+              Local; to review and reverse practices that keep power and
+              decision-making in the hands of the executive; to protect the
+              solvency and legacy of our union; to support our members in their
+              fight to secure a better living and greater security for
+              themselves and their families; and to promote our Local’s values
+              by supporting political candidates and legislation that advance
+              economic, racial, and environmental justice in our Commonwealth.
+            </p>
+            <p className="indent-5">
+              If you want a more strongly democratic, truly participatory,
+              member-led Local, I’m your candidate. We are the union, and
+              together we should lead it. As the Local’s president, I will be a
+              leader who listens and acts based on the needs, the wisdom, and
+              the goals of our members. In this crucial moment in labor history,
+              public support for unions is higher than it's been in years, and
+              workers are gaining exciting victories around the country. My
+              campaign represents a movement within our Local committed to
+              transparency, democracy, member involvement and fighting to win.
+              Join that movement and vote for me as the next president of Local
+              509.
+            </p>
+          </div>
         </div>
       </div>
     </div>
