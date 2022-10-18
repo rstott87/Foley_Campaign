e6d910c39dc9f19ddf5035d2f6e90a414e84893b
rstott87 <rstott87@yahoo.com>
2022-10-18 13:21:39 -0400

    tweaks to borders and spacing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import SocialsContainer from "./SocialsContainer";
 
 function NavBar(props) {
   return (
-    <div className="sticky top-0 z-50 flex justify-between bg-slate-200 px-3 py-2 opacity-95 shadow-lg shadow-slate-800">
+    <div className="nav-bar sticky top-0 z-50 flex justify-between bg-slate-200 px-3 py-2 opacity-95 shadow-lg shadow-slate-800 border-b-2 border-slate-800">
       <SocialsContainer />
       <MenuButton menuOpen={props.menuOpen} menuClickHandler={props.menuClickHandler} />
     </div>
